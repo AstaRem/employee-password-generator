eac3214317dfa59b084aa3e24dbb45a1c50d39da
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-03-19 11:54:09 +0000

    proof that my for loop is not working as internded,line 147 does not get executed, it runs only first part of code

--- script.js
+++ script.js
@@ -96,30 +96,61 @@ let answerLowerC;
 let answerUpperC;
 let answerNum;
 let answerSpecialChar;
+let answers = {};
 function getPasswordOptions() {
+  answerSpecialChar = confirm("Would you like your password to have special characters? ");
+  answerNum = confirm("Would you like your password to have numbers? ");
   answerLowerC = confirm("Would you like your password to have lowercases? ");
   answerUpperC = confirm("Would you like your password to have uppercases? ");
-  answerNum = confirm("Would you like your password to have numbers? ");
-  answerSpecialChar = confirm("Would you like your password to have special characters? ");
-  return [answerLowerC, answerUpperC, answerNum, answerSpecialChar];
+  return answers = {
+    specialChar: answerSpecialChar, 
+    num: answerNum, 
+    lowerC: answerLowerC, 
+    upperC: answerUpperC} ;
 }
 
 getPasswordOptions();
+console.log(answers);
 
 
 let randomIndex;
+let passwordString = "";
 for(let i = 0; i < arr.length; i++){
-  if(answerLowerC){
+  if(answers.specialChar){
     randomIndex = Math.floor(Math.random() * arr[i].length);
-    console.log[`RandomIndex from answerLowerC: ${randomIndex}`];
-    console.log(`value from aswerLowerC array: ${arr[i][randomIndex]}`);
-  } else if (answerUpperC){
+    // console.log(`the length of ${arr[i]} is ${arr[i].length}`)
+    // console.log(`RandomIndex from answerSpecialChar: ${randomIndex}`);
+    // console.log(`value from answerSpecialChar array: ${arr[i][randomIndex]}`);
+    console.log(randomIndex);
+    console.log(answers.specialChar);
+    specialCharValue = arr[i][randomIndex];
+    passwordString += specialCharValue;
+    // console.log(passwordString);
+  } else if (answers.num){
     randomIndex = Math.floor(Math.random() * arr[i].length);
-    console.log[randomIndex];
-    console.log(arr[i][randomIndex]);
+    numValue = arr[i][randomIndex];
+    passwordString += numValue;
+    console.log(randomIndex);
 
+    // console.log(passwordString);
+  } else if (answers.lowerC){
+    randomIndex = Math.floor(Math.random() * arr[i].length);
+    lowerCValue = arr[i][randomIndex];
+    passwordString += lowerCValue;
+    console.log(randomIndex);
+
+    // console.log(passwordString);
+  } else if(answers.upperC){
+    randomIndex = Math.floor(Math.random() * arr[i].length);
+    upperCValue = arr[i][randomIndex];
+    passwordString += upperCValue;
+    console.log(randomIndex);
+    console.log(passwordString);
+
+    // console.log(passwordString);
   }
 }
+
 
 
 // Function for getting a random element from an array
