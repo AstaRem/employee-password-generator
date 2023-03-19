9f8f2472815cb3422a29816adadef96260a9f2bb
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-03-19 12:30:34 +0000

    loop in gitRandom() iterating through correct array now and working

--- script.js
+++ script.js
@@ -96,77 +96,36 @@ let answerLowerC;
 let answerUpperC;
 let answerNum;
 let answerSpecialChar;
-let answers = {};
+let answers = [];
 function getPasswordOptions() {
   answerSpecialChar = confirm("Would you like your password to have special characters? ");
   answerNum = confirm("Would you like your password to have numbers? ");
   answerLowerC = confirm("Would you like your password to have lowercases? ");
   answerUpperC = confirm("Would you like your password to have uppercases? ");
-  return answers = {
-    specialChar: answerSpecialChar, 
-    num: answerNum, 
-    lowerC: answerLowerC, 
-    upperC: answerUpperC} ;
+  return answers = [answerSpecialChar, answerNum, answerLowerC, answerUpperC];
 }
-
 getPasswordOptions();
 console.log(answers);
 
 
-let randomIndex;
-let passwordString = "";
-for(let i = 0; i < arr.length; i++){
-  if(answers.specialChar){
-    randomIndex = Math.floor(Math.random() * arr[i].length);
-    // console.log(`the length of ${arr[i]} is ${arr[i].length}`)
-    // console.log(`RandomIndex from answerSpecialChar: ${randomIndex}`);
-    // console.log(`value from answerSpecialChar array: ${arr[i][randomIndex]}`);
-    console.log(randomIndex);
-    console.log(answers.specialChar);
-    specialCharValue = arr[i][randomIndex];
-    passwordString += specialCharValue;
-    // console.log(passwordString);
-  } else if (answers.num){
-    randomIndex = Math.floor(Math.random() * arr[i].length);
-    numValue = arr[i][randomIndex];
-    passwordString += numValue;
-    console.log(randomIndex);
-
-    // console.log(passwordString);
-  } else if (answers.lowerC){
-    randomIndex = Math.floor(Math.random() * arr[i].length);
-    lowerCValue = arr[i][randomIndex];
-    passwordString += lowerCValue;
-    console.log(randomIndex);
-
-    // console.log(passwordString);
-  } else if(answers.upperC){
-    randomIndex = Math.floor(Math.random() * arr[i].length);
-    upperCValue = arr[i][randomIndex];
-    passwordString += upperCValue;
-    console.log(randomIndex);
-    console.log(passwordString);
-
-    // console.log(passwordString);
-  }
-}
-
-
-
 // Function for getting a random element from an array
-// function getRandom(arr) {
-//   let randomIndex;
-//   for(let i = 0; i < 4; i++){
-//     if(answerLowerC){
-//       randomIndex = Math.floor(Math.random() * 4);
-//       console.log(arr[randomIndex]);
+function getRandom(answers) {
+  let randomIndex;
+  let passwordString = "";
+  for(let i = 0; i < answers.length; i++){
+    if(answers[i]){
+      randomIndex = Math.floor(Math.random() * arr[i].length);
+      console.log(randomIndex);
+      symbolValue = arr[i][randomIndex];
+      passwordString += symbolValue;
+      console.log(passwordString);
+    } 
+    
+  }
+   
+  }
 
-//     }
-//     // return arr[randomIndex]  // returns random value from array
-  
-//   }
-// }
-
+  getRandom(answers);
 
 // Function to generate password with user input
 function generatePassword(arr) {
